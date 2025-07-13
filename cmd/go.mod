ec74a0120aa3eb16f9c864250c85b1824be1d827
Vitalii Savitskii <savitskiy.vitaliy.m@gmail.com>
2025-07-13 12:37:03 +0200

    Initialize the main package and implement the help

---
+++ cmd/go.mod
@@ -0,0 +1,5 @@
+module s-vitaliy/kubectl-plugin-arcane
+
+go 1.24.5
+
+require github.com/alecthomas/kong v1.12.0 // indirect
--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"os"
+
+	"github.com/alecthomas/kong"
+)
+
+var CLI struct {
+	Stream struct {
+		Suspend struct {
+			ID string `arg:"" help:"The ID of the stream to suspend."`
+		} `cmd:"" help:"Suspends the given stream."`
+		Backfill struct {
+			ID string `arg:"" help:"The ID of the stream to backfill."`
+		} `cmd:"" help:"Restarts the given stream in the backfill mode."`
+		Restart struct {
+			ID string `arg:"" help:"The ID of the stream to restart."`
+		} `cmd:"" help:"Restarts the given stream in the streaming mode."`
+	} `cmd:"" help:"Stream operation."`
+}
+
+const AppDescription = "A command line tool for managing the Arcane streams."
 
 func main() {
-	fmt.Println("Hello, world!")
+	executableName := getExecutableName()
+	ctx := kong.Parse(&CLI, kong.Name(executableName), kong.Description(AppDescription))
+	println("Parsed context:", ctx)
+}
+
+func getExecutableName() string {
+	// Not checking for errors here since argv[0] should always be available
+	return os.Args[0]
 }
